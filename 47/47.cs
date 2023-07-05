578644c6fc813246b9560b661ed694f3f5d23bf2
Arthur Maslauskas <mickeyarthurfirst@gmail.com>
2023-07-05 17:44:18 +1100

    Complete 47

--- 47/47.cs
+++ 47/47.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+using System;
+using static System.Console;
+
+WriteLine("Write LINE: ");
+int line1 = Convert.ToInt32(ReadLine());
+WriteLine("Write Colums1: ");
+int columns1 = Convert.ToInt32(ReadLine());
+
+double[,] numbers = new double[line1, columns1];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        Write("]");
+        WriteLine(" ");
+    }
+}
